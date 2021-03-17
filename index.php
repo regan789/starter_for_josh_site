cb70bccc1ce14fbdbb9074d2a8a2d6dbf86f2d46
regan789 <79519340+regan789@users.noreply.github.com>
2021-03-17 17:22:22 +0800

    522pm

--- index.php
+++ index.php
@@ -1,61 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;800&display=swap" rel="stylesheet">
-
-</head>
-
-<body>
-
-    <header class="container-fluid herobg">
-
-        <nav class="navbar">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="a"><span class="JOSH">JOSH</span> <span class="orange">WHITKIN</span></a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">Publications</a></li>
-                        <li><a href="#">CV</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-
-                </div>
-            </div>
-        </nav>
-
-
-        <div class="container">
-            <h1>Games Design For The Future</h1>
-        </div>
-    </header>
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
 
     <section class="container-fluid aboutbg text-center">
         <div class="container">
@@ -81,7 +24,7 @@
                 </div>
 
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="portait" src="Images/portrate.jpg"> 
+                    <img class="img-responsive" alt="portait" src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/portrait.jpg"> 
                          
                     <h4>A PORTRAIT</h4>  
                     <p class="date">12 Dcember 2020</p>
@@ -92,7 +35,7 @@
                 </div>
 
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="portait" src="Images/model.jpg">
+                    <img class="img-responsive" alt="portait" src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/model.jpg">
                     
                     <h4>A MODEL</h4>
                     <p class="date">12 Dcember 2020</p>
@@ -103,7 +46,7 @@
                 </div>
 
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="portait" src="Images/games.jpg">
+                    <img class="img-responsive" alt="portait" src="http://206.189.45.97/~mesh2/wp-content/themes/starter_for_josh_site/images/game.jpg">
                     
                     <h4>GAMES DESIGN</h4>
                     <p class="date">12 Dcember 2020</p>
@@ -122,31 +65,4 @@
 
 
 
-
-    <footer class="container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <p>JOSH WHITKIN</p>
-                </div>
-                <div class="col-md-6">
-                    <p>TERMS AND CONDITIONS</p>
-                </div>
-
-            </div>
-            <!--            row-->
-
-        </div>
-        <!--            container-->
-
-    </footer>
-
-
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
-
-</html>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>  
